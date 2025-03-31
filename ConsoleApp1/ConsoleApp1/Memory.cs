d57297fffc968bd16c03aae3a158a5397f82e73f
galdannn <galdannn0@gmail.com>
2025-03-31 16:31:24 +0800

    mr, mc, mca

--- ConsoleApp1/ConsoleApp1/Memory.cs
+++ ConsoleApp1/ConsoleApp1/Memory.cs
@@ -38,6 +38,28 @@ namespace Calculator
                 Console.WriteLine($"Subtracted {value} from memory at index {index}. New value: {memoryItems[index].Value}");
             }
         }
+        public double Recall(int index)
+        {
+            if (IsValidIndex(index))
+            {
+                Console.WriteLine($"Recalled value at index {index}: {memoryItems[index].Value}");
+                return memoryItems[index].Value;
+            }
+            return 0;
+        }
+        public void Clear(int index)
+        {
+            if (IsValidIndex(index))
+            {
+                Console.WriteLine($"Cleared memory at index {index}");
+                memoryItems.RemoveAt(index);
+            }
+        }
+        public void ClearAll()
+        {
+            memoryItems.Clear();
+            Console.WriteLine("Memory cleared.");
+        }
         public void DisplayMemory()
         {
             if (memoryItems.Count == 0)
