3c538840319886f225c6241853ddd0ab6594e1c9
galdannn <galdannn0@gmail.com>
2025-03-31 16:41:42 +0800

    basicCal

--- ConsoleApp1/ConsoleApp1/BasicCalculator.cs
+++ ConsoleApp1/ConsoleApp1/BasicCalculator.cs
@@ -13,17 +13,49 @@ namespace Calculator
         {
             memory = new Memory();
         }
+
         public void Add(double value)
         {
             Result += value;
-            memory.Store(Result);
+            Console.WriteLine($"Result: {Result}");
         }
-        public void Subtract(double value) { 
+
+        public void Subtract(double value)
+        {
             Result -= value;
-            memory.Store(Result);
-
-
+            Console.WriteLine($"Result: {Result}");
         }
+
+        public void StoreToMemory()  // MS
+        {
+            memory.Store(Result);
+        }
+
+        public void AddToMemory(int index, double value) // M+
+        {
+            memory.AddToMemory(index, value);
+        }
+
+        public void SubtractFromMemory(int index, double value) // M-
+        {
+            memory.SubtractFromMemory(index, value);
+        }
+
+        public void RecallMemory(int index) // MR
+        {
+            Result = memory.Recall(index);
+        }
+
+        public void ClearMemory(int index) // MC (specific item)
+        {
+            memory.Clear(index);
+        }
+
+        public void ClearAllMemory() // MC (all)
+        {
+            memory.ClearAll();
+        }
+
         public void ShowMemory()
         {
             memory.DisplayMemory();
